b7058fa70701cdc3836b1f20a28468c9a4d2083f
rahulgoyal26 <himrrahulgoyal@gmail.com>
2021-02-13 20:33:57 +0530

    Add Two Numbers via function

---
+++ Functions/Add Two Numbers.py
@@ -0,0 +1,15 @@
+# creating the function 
+
+def add(a, b):
+    print("This is the sum", a+b)
+
+# Getting the inputs from the user
+
+add(3, 7)
+
+a = input("Enter your first number ")
+b = input("Enter your second number ") 
+
+# calling the function 
+
+add(int(a), int(b))
